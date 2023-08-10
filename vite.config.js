723b9eaba7c8c6d083f68d043d6171e4efd6ab34
yend724 <yend9524@gmail.com>
2023-08-10 16:54:26 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,33 +1,23 @@
 import path from 'path';
-// vite.config.js
+import fg from 'fast-glob';
+
 const dirname = path.dirname(new URL(import.meta.url).pathname);
+const paths = await fg.glob(path.resolve(dirname, 'src') + '/**/*.html');
+const inputs = paths.reduce((acc, cur) => {
+  const key = cur
+    .replace(`${path.resolve(dirname, 'src')}/`, '')
+    .replace('.html', '');
+  acc[key] = cur;
+  return acc;
+}, {});
+
+// vite.config.js
 export default {
   root: path.resolve(dirname, 'src'),
   build: {
     outDir: path.resolve(dirname, 'dist'),
     rollupOptions: {
-      input: {
-        'view-transitions-api': path.resolve(
-          dirname,
-          'src',
-          'view-transitions-api/index.html'
-        ),
-        'view-transitions-api/detail/01': path.resolve(
-          dirname,
-          'src',
-          'view-transitions-api/detail/01/index.html'
-        ),
-        'view-transitions-api/detail/02': path.resolve(
-          dirname,
-          'src',
-          'view-transitions-api/detail/02/index.html'
-        ),
-        'view-transitions-api/detail/03': path.resolve(
-          dirname,
-          'src',
-          'view-transitions-api/detail/03/index.html'
-        ),
-      },
+      input: inputs,
     },
   },
 };
