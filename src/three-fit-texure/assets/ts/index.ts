779b3f924e7aa30072e57f8e90af58862b8c3bf0
yend724 <yend9524@gmail.com>
2024-01-18 16:44:53 +0900

    add comment

--- src/three-fit-texure/assets/ts/index.ts
+++ src/three-fit-texure/assets/ts/index.ts
@@ -5,6 +5,8 @@ const canvas = document.querySelector<HTMLCanvasElement>('#canvas');
 if (!canvas) {
   throw new Error('canvas not found');
 }
+
+// windowのサイズを取得
 const getWindowSize = () => {
   const width = window.innerWidth;
   const height = window.innerHeight;
@@ -15,20 +17,19 @@ const getWindowSize = () => {
     aspect,
   };
 };
-const getShader = (id: string) => {
-  const script = document.querySelector<HTMLScriptElement>(`#${id}`);
+// shaderを取得
+const getShader = (type: 'fragmentShader' | 'vertexShader') => {
+  const script = document.querySelector<HTMLScriptElement>(`#${type}`);
   if (!script) {
-    throw new Error(`script not found: ${id}`);
+    throw new Error(`script not found: ${type}`);
   }
   return script.innerText;
 };
 
-const FragmentShader = getShader('fragmentShader');
-const VertexShader = getShader('vertexShader');
-
 const app = (texture: THREE.Texture) => {
   const windowSize = getWindowSize();
 
+  // rendererの作成
   const renderer = new THREE.WebGLRenderer({
     canvas,
   });
@@ -36,8 +37,11 @@ const app = (texture: THREE.Texture) => {
   renderer.setPixelRatio(window.devicePixelRatio);
   renderer.setSize(windowSize.width, windowSize.height);
 
+  // sceneの作成
   const scene = new THREE.Scene();
 
+  // cameraの作成
+  // OrthographicCameraでwindowサイズを指定
   const camera = new THREE.OrthographicCamera(
     windowSize.width / -2,
     windowSize.width / 2,
@@ -46,27 +50,32 @@ const app = (texture: THREE.Texture) => {
     1,
     10
   );
+  // cameraの位置を指定
   camera.position.z = 5;
   scene.add(camera);
 
+  // textureのアスペクト比を取得
   const textureImg = texture.image as HTMLImageElement;
-  const textureWidth = textureImg.width;
-  const textureHeight = textureImg.height;
+  const textureAspect = textureImg.width / textureImg.height;
 
+  const FragmentShader = getShader('fragmentShader');
+  const VertexShader = getShader('vertexShader');
+
+  // uniformの定義
   const uniforms = {
     uTexture: {
       value: texture,
     },
     uTextureAspect: {
-      value: textureWidth / textureHeight,
+      value: textureAspect,
     },
     uScreenAspect: {
       value: windowSize.aspect,
     },
   };
+  // planeの作成
   const geometry = new THREE.PlaneGeometry(windowSize.width, windowSize.height);
   const material = new THREE.ShaderMaterial({
-    side: THREE.DoubleSide,
     uniforms,
     vertexShader: VertexShader,
     fragmentShader: FragmentShader,
@@ -74,29 +83,34 @@ const app = (texture: THREE.Texture) => {
   const plane = new THREE.Mesh(geometry, material);
   scene.add(plane);
 
+  // requestAnimationFrameで描画
   const tick = () => {
     requestAnimationFrame(tick);
-
     renderer.render(scene, camera);
   };
   tick();
 
+  // windowのリサイズ処理
   const onResize = () => {
     const windowSize = getWindowSize();
 
+    // planeのサイズをwindowのサイズに合わせる
     plane.geometry = new THREE.PlaneGeometry(
       windowSize.width,
       windowSize.height
     );
 
+    // uniformで渡しているwindowのアスペクト比を更新
     material.uniforms.uScreenAspect.value = windowSize.aspect;
 
+    // cameraを更新
     camera.left = windowSize.width / -2;
     camera.right = windowSize.width / 2;
     camera.top = windowSize.height / 2;
     camera.bottom = windowSize.height / -2;
     camera.updateProjectionMatrix();
 
+    // rendererを更新
     renderer.setPixelRatio(window.devicePixelRatio);
     renderer.setSize(windowSize.width, windowSize.height);
   };
