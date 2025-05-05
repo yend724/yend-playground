2ed9812bfa1a9761f6e104aeec8b1881aece727b
yend724 <yend9524@gmail.com>
2025-05-05 22:29:22 +0900

    wip

--- src/boids-simulation/features/boid-simulation/index.ts
+++ src/boids-simulation/features/boid-simulation/index.ts
@@ -49,13 +49,14 @@ export class BoidSimulation {
 
   /** 群れの重心を計算 */
   #calcFlockCenter(): { x: number; y: number } {
-    let x = 0,
-      y = 0;
+    let x = 0;
+    let y = 0;
+    const boidsCount = this.#boids.length;
     for (const b of this.#boids) {
       x += b.position.x;
       y += b.position.y;
     }
-    return { x: x / BOID_COUNT, y: y / BOID_COUNT };
+    return { x: x / boidsCount, y: y / boidsCount };
   }
 
   /** 1フレーム分の実行 */
