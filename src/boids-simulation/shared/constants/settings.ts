8ecb849ed7eccd9cf91ad932987bc8c3ee861520
yend724 <yend9524@gmail.com>
2025-05-05 18:59:08 +0900

    wip

--- src/boids-simulation/shared/constants/settings.ts
+++ src/boids-simulation/shared/constants/settings.ts
@@ -1,6 +1,6 @@
-export const BOID_COUNT = 1_000;
+export const BOID_COUNT = 100;
 export const BOID_VELOCITY = 2;
 export const DISTANCE_TO_NEIGHBOR = 20;
-export const COHESION_WEIGHT = 0.01;
+export const COHESION_WEIGHT = 0.02;
 export const ALIGNMENT_WEIGHT = 0.1;
-export const SEPARATION_WEIGHT = 0.2;
+export const SEPARATION_WEIGHT = 0.25;
